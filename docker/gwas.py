6d0e44dfa4214d16eaa01dd2d34b4cca790457aa
joyvalley <jan.freudenthal@uni-wuerzburg.de>
2019-12-18 16:04:28 +0100

    Add functions for manhattanplot

--- docker/gwas.py
+++ docker/gwas.py
@@ -16,10 +16,10 @@ mac_min= 6
 
 X_file = 'gwas_sample_data/bla.plink'
 Y_file = 'gwas_sample_data/pheno2.csv'
-K_file = 'gwas_sample_data/kinship_ibs_binary_mac5.h5py'
+K_file = 'not_prov'
 cof_file = 0 
 cof = "nan"
-
+plot = False 
 
 
 for i in range (1,len(sys.argv),2):
@@ -40,7 +40,9 @@ for i in range (1,len(sys.argv),2):
     elif sys.argv[i] == "-p" or sys.argv[i] == "--perm":
         perm  = int(sys.argv[i+1])
     elif sys.argv[i] == "-o" or sys.argv[i] == "--out":
-        out_file = sys.argv[i+1]       
+        out_file = sys.argv[i+1]
+    elif sys.argv[i] == "--plot":
+        plot = bool(sys.argv[i+1])
     elif sys.argv[i] == "-h" or sys.argv[i] == "--help":
         print("-x , --genotype :file containing marker information in csv or hdf5 format of size")
         print("-y , --phenotype: file container phenotype information in csv format"  )
@@ -71,31 +73,33 @@ markers_used , X , macs = main.mac_filter(mac_min,X,markers)
 ## prepare
 print("Begin performing GWAS on ", Y_file)
 
-if perm == 1:
-    output = main.gwas(X,K,Y_,batch_size,cof)   
-    if( X_file.split(".")[-1] == 'csv'):
-        chr_pos = np.array(list(map(lambda x : x.split("- "),markers_used)))
-    elif X_file.split(".")[-1].lower() == 'plink':
-         my_chr = [i.split("r")[1] for i in [i.split("_")[0] for i in  markers_used]]
-         my_pos = [i.split("_")[1] for i in  markers_used]
-         chr_pos = np.vstack((my_chr,my_pos)).T      
-    else: 
-        chr_reg = h5py.File(X_file,'r')['positions'].attrs['chr_regions']
-        mk_index= np.array(range(len(markers)),dtype=int)[macs >= mac_min]
-        chr_pos = np.array([list(map(lambda x: sum(x > chr_reg[:,1]) + 1, mk_index)), markers_used]).T
-        my_time = np.repeat((time.time()-start),len(chr_pos))
-    pd.DataFrame({
-        'chr' : chr_pos[:,0] ,
-        'pos' : chr_pos[:,1] , 
-        'pval': output[:,0] ,
-        'mac' : np.array(macs[macs >= mac_min],dtype=np.int) ,
-        'eff_size': output[:,1] ,
-        'SE' : output[:,2]}).to_csv(out_file,index=False)
-elif perm > 1:
+
+output = main.gwas(X,K,Y_,batch_size,cof)   
+if( X_file.split(".")[-1] == 'csv'):
+    chr_pos = np.array(list(map(lambda x : x.split("- "),markers_used)))
+elif X_file.split(".")[-1].lower() == 'plink':
+     my_chr = [i.split("r")[1] for i in [i.split("_")[0] for i in  markers_used]]
+     my_pos = [i.split("_")[1] for i in  markers_used]
+     chr_pos = np.vstack((my_chr,my_pos)).T      
+else: 
+    chr_reg = h5py.File(X_file,'r')['positions'].attrs['chr_regions']
+    mk_index= np.array(range(len(markers)),dtype=int)[macs >= mac_min]
+    chr_pos = np.array([list(map(lambda x: sum(x > chr_reg[:,1]) + 1, mk_index)), markers_used]).T
+    my_time = np.repeat((time.time()-start),len(chr_pos))
+res = pd.DataFrame({
+    'chr' : chr_pos[:,0] ,
+    'pos' : chr_pos[:,1] , 
+    'pval': output[:,0] ,
+    'mac' : np.array(macs[macs >= mac_min],dtype=np.int) ,
+    'eff_size': output[:,1] ,
+    'SE' : output[:,2]})
+res.to_csv(out_file,index=False)
+if perm > 1:
     min_pval = []
     perm_seeds = []
     my_time = []
     for i in range(perm):
+        perm_out = 'perm_'+out_file
         start_perm = time.time()
         print("Running permutation ", i+1, " of ",perm)
         my_seed  = np.asscalar(np.random.randint(9999,size=1))
@@ -106,12 +110,19 @@ elif perm > 1:
         min_pval.append(np.min(output[:,0]))
         print("Elapsed time for permuatation",i+1 ," with p_min", min_pval[i]," is",": ", round(time.time() - start_perm,2))
         my_time.append(time.time()-start_perm)
-    pd.DataFrame({
+    res_perm = pd.DataFrame({
         'time': my_time ,
         'seed': perm_seeds ,
-        'min_p': min_pval }).to_csv(out_file,index=False)
+        'min_p': min_pval }).sort_values('min_p')
+    res_perm.to_csv(perm_out,index=False)
 
-print("done")
+
+if plot == True:
+    import plot 
+    plot.manhattan(out_file,perm)
+
+    
+print("Finished performing GWAS")
  
 end = time.time()
 eltime = np.round(end -start,2)
@@ -122,6 +133,4 @@ elif eltime > 59 and eltime <= 3600:
     print("Total time elapsed",  np.round(eltime / 60,2) , "minutes")
 elif eltime > 3600 :
     print("Total time elapsed",  np.round(eltime / 60 / 60,2), "hours")
-
 end = time.time()
-
