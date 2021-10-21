bfc5cfe5a246d8efa794940b5683fe2e19571e15
arthurkorte <arthur.korte@gmail.com>
2021-10-21 10:25:22 +0200

    some additional fixes

--- gwas_flow/herit.py
+++ gwas_flow/herit.py
@@ -1,4 +1,5 @@
 ''' custom function from limix package that calculates the variance components estiamtes '''
+import numpy as np
 from numpy_sugar.linalg import economic_qs
 from numpy import pi, var
 from numpy_sugar import is_all_finite
@@ -11,16 +12,14 @@ def estimate_variance_components(y_phe, kin, marker_mat=None, verbose=True):
         raise ValueError("Outcome must have finite values only.")
     if not is_all_finite(kin):
         raise ValueError("Outcome must have finite values only.")
-    if marker_mat is not None:
-        if not is_all_finite(marker_mat):
-                raise ValueError("Outcome must have finite values only.")
+    marker_mat = np.full ((y_phe.shape[0],1),1.) 
     if kin is not None:
            # K = K / diag(K).mean()
             q_s = economic_qs(kin)
     else:
             q_s = None
        
-    method = LMM(y_phe.values, marker_mat.values, q_s, restricted=True)
+    method = LMM(y_phe, marker_mat, q_s, restricted=True)
     method.fit(verbose=verbose)
         
     v_g = method.scale * (1 - method.delta)
